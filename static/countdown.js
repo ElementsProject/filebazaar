f66b6d6b42e650ca9a12d09ecd67bbda45de30e6
Nadav Ivgi <nadav@shesek.info>
2017-12-18 01:38:12 +0200

    Add countdown timer to invoice and access token.

---
+++ static/countdown.js
@@ -0,0 +1,21 @@
+(function(){
+
+  document.querySelectorAll('[data-countdown-to]').forEach(el => {
+    var parent  = el.parentNode
+      , expires = +el.dataset.countdownTo
+
+    function update() {
+      var left = expires - (+new Date()/1000|0)
+      if (left > 0) el.innerHTML = 'in ' + formatDur(left)
+      else location.reload()
+    }
+    update()
+    setInterval(update, 1000)
+  })
+
+  function formatDur(x) {
+    var h=x/3600|0, m=x%3600/60|0, s=x%60
+    return ''+(h>0?h+':':'')+(m<10&&h>0?'0':'')+m+':'+(s<10?'0':'')+s
+  }
+
+})()
